d217cdb5cfd7c9f644d614624695cfa85f591879
Irvine <fleith.irvine@gmail.com>
2025-05-08 17:44:08 +0200

    Init monorepo fullstack EcoFleet

---
+++ backend/src/test/java/fr/irvineflth/EcoFleet/EcoFleetApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.irvineflth.EcoFleet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoFleetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
