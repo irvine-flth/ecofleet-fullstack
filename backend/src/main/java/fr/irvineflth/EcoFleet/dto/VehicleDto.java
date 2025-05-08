279a7f23cf95b2d3db1b0598f3339fe6a6a77f83
Irvine <fleith.irvine2@gmail.com>
2025-05-08 18:10:29 +0200

    feat(vehicle): created DTO VehicleDto

---
+++ backend/src/main/java/fr/irvineflth/EcoFleet/dto/VehicleDto.java
@@ -0,0 +1,25 @@
+package fr.irvineflth.EcoFleet.dto;
+
+import fr.irvineflth.EcoFleet.domain.enums.VehicleType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VehicleDto {
+    private Long id;
+    private String registrationNumber;
+    private VehicleType type;
+    private String brand;
+    private String model;
+    private Integer autonomyKm;
+    private Integer currentKm;
+    private LocalDate lastMaintenanceDate;
+    private boolean isAvailable;
+}
